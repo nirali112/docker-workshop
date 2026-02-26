2804be266095ac1d2c491fb4b2c4f1856c7d1fe7
Nirali Mehta <61585082+nirali112@users.noreply.github.com>
2026-02-26 07:55:21 +0000

    pipleline files

---
+++ pipeline/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+import pandas as pd
+
+print("Hello, World!",sys.argv)
+
+month = int(sys.argv[1])
+print(f"The month is: {month}")
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month'] = month
+print(df.head())
+
+df.to_parquet(f"output_day_{month}.parquet")
